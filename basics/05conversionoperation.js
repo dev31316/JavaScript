c5486ef05a6093f994a96f19ee7aea17796a57b5
Hasib Raihan <dsillusiongamer@gmail.com>
2026-04-09 14:53:27 +0000

    conversion

--- basics/05conversionoperation.js
+++ basics/05conversionoperation.js
@@ -1,2 +1,14 @@
-let game = 'sdhhsf';
-console.log(game);
+let isLoggedIn = "Himel"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
